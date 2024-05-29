bf3df9667be39f457f8b901ff841bf6ad08bd11e
Yuxuan <yuxuan_zhang13@outlook.com>
2024-05-29 01:09:32 -0700

    modify setup and update read me

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ requirements = [
     'matplotlib>=3.9.0',
     'pandas>=2.2.2',
     'tabulate'
-    
 ]
 
 def setup_package():
@@ -17,10 +16,10 @@ def setup_package():
         name='dependency-installer',
         version='0.1.0',
         description='Install project dependencies',
+        py_modules=[],  # Specify no modules to prevent package discovery
         install_requires=requirements,
         zip_safe=False,
     )
 
 if __name__ == '__main__':
     setup_package()
-
